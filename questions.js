969408c75c77abb9d3b32fe1cf8a92f6024ae60c
Inna Widener <iwide2019@gmail.com>
2020-07-05 22:36:50 -0500

    Uncommented the questions in file.

--- questions.js
+++ questions.js
@@ -4,36 +4,36 @@ var questions = [
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
-    // {
-    //     title: "The condition in an if / else statement is enclosed within ____.",
-    //     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    //     answer: "parentheses"
-    // },
-    // {
-    //     title: "Inside which HTML element do we put the JavaScript?",
-    //     choices: ["js", "javascript", "scripting", "script"],
-    //     answer: "script"
-    // },
-    // {
-    //     title: "Which event occurs when the user clicks on an HTML element?",
-    //     choices: ["onmouseclick", "onmouseover", "onchange", "onclick"],
-    //     answer: "onclick"
-    // },
-    // {
-    //     title: "How do you declare a Javascript variable?",
-    //     choices: ["variable carName", "var carName", "v carName", "const carname"],
-    //     answer: "var carName"
-    // },
-    // {
-    //     title: "How do you round the number 7.25, to the nearest integer?",
-    //     choices: ["Math.round(7.25)", "rnd(7.25)", "Math.rnd(7.25)", "round(7.25)"],
-    //     answer: "Math.round(7.25)"
-    // },
-    // {
-    //     title: "What does CSS stand for?",
-    //     choices: ["Computer Style Sheets", "Creative Style Sheets", "Colorful Style Sheets", "Cascading Style Sheets"],
-    //     answer: "Cascading Style Sheets"
-    // },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["js", "javascript", "scripting", "script"],
+        answer: "script"
+    },
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseclick", "onmouseover", "onchange", "onclick"],
+        answer: "onclick"
+    },
+    {
+        title: "How do you declare a Javascript variable?",
+        choices: ["variable carName", "var carName", "v carName", "const carname"],
+        answer: "var carName"
+    },
+    {
+        title: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["Math.round(7.25)", "rnd(7.25)", "Math.rnd(7.25)", "round(7.25)"],
+        answer: "Math.round(7.25)"
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Computer Style Sheets", "Creative Style Sheets", "Colorful Style Sheets", "Cascading Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
     {
         title: "Which HTML attribute is used to define inline styles?",
         choices: ["class", "font", "styles", "style"],
